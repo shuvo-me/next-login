9082af0fc23d44e9209b2f1e8f3f52bff5294b49
shuvo-me <shuvo.js.dev@gmail.com>
2023-04-03 19:22:28 +0600

    fix login page error

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,34 +1,32 @@
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
-import { FaTwitter } from "react-icons/fa";
+// import { FaTwitter } from "react-icons/fa";
 
 import { VscGithubInverted } from "react-icons/vsc";
 const Login = () => {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-white max-w-[700px] mx-auto mt-[100px] p-10">
-        <div className="bg-slate-50 flex flex-col justify-center gap-3 p-[50px] px-[200px]">
-          <button
-            onClick={() => signIn("google", { callbackUrl: "/" })}
-            className="bg-white px-4 py-4 flex items-center gap-2"
-          >
-            <span className="text-[1.5rem]">
-              <FcGoogle />
-            </span>
-            <span>SignIn With Google</span>
-          </button>
-          <button
-            onClick={() => signIn("github", { callbackUrl: "/" })}
-            className="bg-white px-4 py-4 flex items-center gap-2"
-          >
-            <span className="text-[1.5rem]">
-              <VscGithubInverted />
-            </span>
-            <span>SignIn With Github</span>
-          </button>
-          {/* <button
+  return (
+    <div className="bg-white max-w-[700px] mx-auto mt-[100px] p-10">
+      <div className="bg-slate-50 flex flex-col justify-center gap-3 p-[50px] px-[200px]">
+        <button
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="bg-white px-4 py-4 flex items-center gap-2"
+        >
+          <span className="text-[1.5rem]">
+            <FcGoogle />
+          </span>
+          <span>SignIn With Google</span>
+        </button>
+        <button
+          onClick={() => signIn("github", { callbackUrl: "/" })}
+          className="bg-white px-4 py-4 flex items-center gap-2"
+        >
+          <span className="text-[1.5rem]">
+            <VscGithubInverted />
+          </span>
+          <span>SignIn With Github</span>
+        </button>
+        {/* <button
             onClick={() => signIn("twitter", { callbackUrl: "/" })}
             className="bg-white px-4 py-4 flex items-center gap-2"
           >
@@ -37,10 +35,9 @@ const Login = () => {
             </span>
             <span>SignIn With Twitter</span>
           </button> */}
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Login;
